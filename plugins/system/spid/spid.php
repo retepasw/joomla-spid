6759b90476b4668a76671659827b23a64744daaa
simeucci <simone.meucci@meoweb.net>
2019-02-15 17:44:01 +0100

    Update spid.php
    Per qualunque tipo di errore, a partire dall'annullamento dell'utente, ricevevamo l'errore generico
    PLG_SYSTEM_SPID_ERRORCODE_UNKNOWN
    
    L'applicazione di questa patch, consente di sollevare gli errori specifici già previsti nei seguenti file di linguaggio:
    PLG_SYSTEM_SPID_ERRORCODE_NR2
    PLG_SYSTEM_SPID_ERRORCODE_NR3
    PLG_SYSTEM_SPID_ERRORCODE_NR12
    PLG_SYSTEM_SPID_ERRORCODE_NR19
    PLG_SYSTEM_SPID_ERRORCODE_NR20
    PLG_SYSTEM_SPID_ERRORCODE_NR21
    PLG_SYSTEM_SPID_ERRORCODE_NR22
    PLG_SYSTEM_SPID_ERRORCODE_NR23
    PLG_SYSTEM_SPID_ERRORCODE_NR25

--- plugins/system/spid/spid.php
+++ plugins/system/spid/spid.php
@@ -180,7 +180,11 @@ class plgSystemSpid extends JPlugin
 			$e = $s['SimpleSAML_Auth_State.exceptionData'];
 			$lang = JFactory::getLanguage();
 			$message = $e->getMessage();
-			$key = 'PLG_SYSTEM_SPID_' . str_replace(' ', '_', strtoupper($message));
+            		$MESSAGE = str_replace(' ', '_', strtoupper($message));
+            		$error_suffix = ( strpos( $MESSAGE, '_ERRORCODE_NR' ) !== false )
+                            ? substr( $MESSAGE , ( strpos( $MESSAGE, '_ERRORCODE_NR' ) + 1 ) )
+                            : $MESSAGE;
+			$key = 'PLG_SYSTEM_SPID_' . $error_suffix;
 			JLog::add(
 					new JLogEntry($lang->hasKey($key) ? JText::_($key) : JText::sprintf('PLG_SYSTEM_SPID_ERRORCODE_UNKNOWN', $message), JLog::WARNING,
 							'plg_system_spid'));
